d39349e49a512095238531831682cdf3045adcc5
Markus Yaseen <MarkusY@ppedv.de>
2019-04-23 16:54:51 +0200

    Eigene Dictionary Klasse mit IEnumerable Interface und dem Generic IEnumerable

---
+++ ListenProgramm/Datenwörterbuch.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenProgramm
+{
+    class Datenwörterbuch: IEnumerable<InternalArrayType>
+    {
+        public int[] Keys
+        {
+            get
+            {
+                var keyArray = new int[_internalArray.Length];
+                for (int i = 0; i < _internalArray.Length; i++)
+                {
+                    keyArray[i] = _internalArray[i].key;
+                }
+                    return keyArray;
+            }
+        }
+        public string[] Values
+        {
+            get
+            {
+                var valueArray = new string[_internalArray.Length];
+                for (int i = 0; i < _internalArray.Length; i++)
+                {
+                    valueArray[i] = _internalArray[i].value;
+                }
+                return valueArray;
+            }
+        }
+
+        //public string[] Values {get; private set;}
+        private InternalArrayType[] _internalArray;
+
+        public void Add(int key, string value)
+        {
+            if (_internalArray == null)
+            {
+                _internalArray = new InternalArrayType[1];
+                _internalArray[0].key = key;
+                _internalArray[0].value = value;
+            } else
+            {
+                var _internalArrayCopy = new InternalArrayType[_internalArray.Length + 1];
+                // Ohne Kopierkonstruktor
+                for (int i = 0; i < _internalArray.Length; i++)
+                {
+                    _internalArrayCopy[i].key = _internalArray[i].key;
+                    _internalArrayCopy[i].value = _internalArray[i].value;
+                }
+                _internalArrayCopy[_internalArray.Length].key = key;
+                _internalArrayCopy[_internalArray.Length].value= value;
+                _internalArray = _internalArrayCopy;
+            }
+        }
+        public void Remove(int key)
+        {
+            var _internalArrayCopy = new InternalArrayType[_internalArray.Length - 1];
+            int j = 0;
+            for(int i = 0; i < _internalArray.Length; i++)
+            {
+                if(_internalArray[i].key == key)
+                {
+                    continue;
+                }
+                _internalArrayCopy[j++] = _internalArray[i];
+            }
+            _internalArray = _internalArrayCopy;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<InternalArrayType> GetEnumerator()
+        {
+            foreach(var item in _internalArray)
+            {
+                yield return item;
+            }
+        }
+
+    
+    }
+    public struct InternalArrayType
+    {
+        public int key;
+        public string value;
+    }
+}
---
+++ ListenProgramm/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integerArray = { 10, 5, 99, 14, 1, 35 };
+            Console.WriteLine("------------------Array mit For---------------------------");
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                Console.WriteLine(integerArray[i]);
+            }
+            Console.WriteLine("------------------Array mit ForEach---------------------------");
+            foreach (var i in integerArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("------------------ArrayList mit ForEach---------------------------");
+            ArrayList integerArrayList = new ArrayList();
+            integerArrayList.Add(10);
+            integerArrayList.Add(5);
+            integerArrayList.Add(99);
+            integerArrayList.Add(14);
+            integerArrayList.Add("abc");
+            integerArrayList.Add(new object());
+            integerArrayList.Add(new Random());
+            integerArrayList.Add(1);
+            integerArrayList.Add(35);
+
+            foreach(var item in integerArrayList)
+            {
+                if(item is int intItem)
+                {
+                    Console.WriteLine(5 + intItem);
+                }
+                
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------------List mit ForEach---------------------------");
+            List<int> integerList = new List<int>();
+            integerList.Add(10);
+            integerList.Add(5);
+            integerList.Add(99);
+            integerList.Add(14);
+            integerList.Add(1);
+            integerList.Add(35);
+
+            Console.WriteLine("------------------Dictionary mit ForEach---------------------------");
+            var dict = new Dictionary<int, string>();
+            dict.Add(10, "Test");
+            dict.Add(9, "Test2");
+            dict.Add(7, "Test3");
+
+            Console.WriteLine(dict.Values);
+
+            Console.WriteLine("------------------Datenwörterbuch mit ForEach---------------------------");
+            Datenwörterbuch dw = new Datenwörterbuch();
+            dw.Add(10, "Test");
+            dw.Add(9, "Test2");
+            dw.Add(7, "Test3");
+            
+            foreach(var item in dw)
+            {
+                Console.WriteLine(item.value);
+            }
+
+            dw.Remove(9);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
