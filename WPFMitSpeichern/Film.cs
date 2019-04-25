3d57478df71806ab3edecb183ee8f3bc6de3a229
Markus Yaseen <MarkusY@ppedv.de>
2019-04-25 16:03:31 +0200

    WPF Eingabe mit Databinding

---
+++ WPFMitSpeichern/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMitSpeichern
+{
+    public class Film
+    {
+        public string Titel { get; set; }
+        public string Länge { get; set; }
+        public string Regisseur { get; set; }
+        public int FSK { get; set; }
+        public bool Vor2000 { get; set; }
+    }
+}
---
+++ WPFMitSpeichern/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMitSpeichern
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Film Film;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Film = new Film();
+            DataContext = Film;
+        }
+
+        private void Speichern_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Film.Titel.ToString());
+            MessageBox.Show(Film.Länge.ToString());
+            MessageBox.Show(Film.Regisseur.ToString());
+            MessageBox.Show(Film.FSK.ToString());
+            MessageBox.Show(Film.Vor2000.ToString());
+        }
+    }
+}
