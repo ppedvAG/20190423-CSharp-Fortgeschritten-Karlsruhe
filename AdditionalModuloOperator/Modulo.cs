537cb3453a4b5911fcb46a50b286ad0612d43e7b
Markus Yaseen <MarkusY@ppedv.de>
2019-04-25 09:32:59 +0200

    Füge Modulo hinzu

---
+++ AdditionalModuloOperator/Modulo.cs
@@ -0,0 +1,17 @@
+﻿using C_Sharp_Fortgeschritten.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdditionalModuloOperator
+{
+    class Modulo : ICalcMethods
+    {
+        public string Method => "%";
+
+        public decimal Do(decimal op1, decimal op2)
+        {
+            return op1 % op2;
+        }
+    }
+}
