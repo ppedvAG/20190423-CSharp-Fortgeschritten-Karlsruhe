27083ededfe8cd2ee4dd4607a79f53c6daf50bb0
Markus Yaseen <MarkusY@ppedv.de>
2019-04-25 14:17:06 +0200

    Async Await Beispiel

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = MachEtwas();
+
+            Console.WriteLine("Ich bin wieder im Main");
+
+            Console.ReadKey();
+        }
+
+        static async Task MachEtwas()
+        {
+            Task<int> machLangsamTask = MachLangsam();
+
+            for(int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            int ergebnis = await machLangsamTask;
+
+            Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        
+        static async Task<int> MachLangsam()
+        {
+            Console.WriteLine("MachLangsam beginnt");
+            await Task.Delay(4000);
+            Console.WriteLine("MachLangsam hat gewartet");
+            return 597;
+        }
+    }
+}
