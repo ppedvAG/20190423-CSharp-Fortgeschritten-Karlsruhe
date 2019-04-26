ddfd9c598f920c9ce1ba58b971dc36735c844c1e
Markus <markus400@hotmail.de>
2019-04-26 18:56:14 +0200

    Füge der WPF Applikation eine GUI mit Auswahl der Filmen hinzu, inklusive Speichern, Öffnen, Löschen und Bearbeiten

---
+++ WPFMitSpeichern/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMitSpeichern
+{
+	public class Filme
+	{
+		// Damit das Databinding funktioniert, muss die Liste eine Property sein
+		public List<Film> FilmListe { get; set; }
+    }
+}
--- WPFMitSpeichern/MainWindow.xaml.cs
+++ WPFMitSpeichern/MainWindow.xaml.cs
@@ -28,27 +28,29 @@ namespace WPFMitSpeichern
     /// </summary>
     public partial class MainWindow : Window
     {
-        Film Film;
+        public Filme Filme;
         public MainWindow()
-        {
-            InitializeComponent();
-            Film = new Film();
-            DataContext = Film;
+		{
+			Filme = new Filme();
+			InitializeComponent();
+			Filme.FilmListe = new List<Film>();
+			DataContext = Filme;
+			UpdateLayout();
         }
 
         private void Speichern_Button_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.FileName = "Film.xml";
+            saveFileDialog.FileName = "Filme.xml";
             saveFileDialog.Filter = "XML|*.xml";
             saveFileDialog.RestoreDirectory = true;
             saveFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
             if (saveFileDialog.ShowDialog() == true)
             {
-                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Film));
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Filme));
                 Stream stream = new FileStream(Path.GetFullPath(saveFileDialog.FileName), FileMode.Create, FileAccess.Write);
                 XmlWriter xmlWriter = XmlWriter.Create(stream);
-                xmlSerializer.Serialize(stream, Film);
+                xmlSerializer.Serialize(stream, Filme);
 
                 stream.Close();
             }
@@ -56,14 +58,65 @@ namespace WPFMitSpeichern
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Oeffnen_Button_Click(object sender, RoutedEventArgs e)
         {
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Film));
-            Stream stream = new FileStream("Film.txt", FileMode.Open, FileAccess.Read);
-            Film = (Film)xmlSerializer.Deserialize(stream);
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.FileName = "Filme.xml";
+			openFileDialog.Filter = "XML|*.xml";
+			openFileDialog.RestoreDirectory = true;
+			openFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
 
-            DataContext = Film;
-            stream.Close();
+			if (openFileDialog.ShowDialog() == true)
+			{
+				XmlSerializer xmlSerializer = new XmlSerializer(typeof(Filme));
+				Stream stream = new FileStream(Path.GetFullPath(openFileDialog.FileName), FileMode.Open, FileAccess.Read);
+				Filme = (Filme)xmlSerializer.Deserialize(stream);
+
+				DataContext = Filme;
+				stream.Close();
+			}
         }
-    }
+
+		// Wir löschen den ausgewählten Eintrag aus der Liste
+		private void Delete_Entry_Button_Click(object sender, RoutedEventArgs e)
+		{
+			if(FilmeListBox.SelectedItem is Film film) {
+				Filme.FilmListe.Remove(film);
+				// Wir müssen der ListBox mitteilen, dass diese neu gerendert wird, ansonsten verändert sich unsere Liste zwar, aber es wird nicht angezeigt
+				FilmeListBox.Items.Refresh();
+			}
+		}
+
+		// Hier müssen wir ein neues Filmobjekt erstellen und es der Liste hinzufügen
+		private void Add_Entry_Button_Click(object sender, RoutedEventArgs e)
+		{
+			Film film = new Film();
+			FilmEditor.DataContext = film;
+			Filme.FilmListe.Add(film);
+
+			// Hier wechseln wir den Fokus auf den neu erstellten Eintrag
+			FilmeListBox.SelectedItem = film;
+			FilmeListBox.Items.Refresh();
+		}
+
+		private void Edit_Entry_Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (FilmeListBox.SelectedItem is Film film)
+			{
+				FilmEditor.DataContext = film;
+			}
+		}
+
+		// Komfortfunktion, damit man nicht immer auf den Button "Eintrag bearbeiten" klicken muss
+		// Wenn wir diese Funktion verwenden, dann brauchen wir den Eintrag bearbeiten Button nicht mehr
+		// Immer wenn etwas selektiert wird, dann wird ein Event geworfen und wir hören hier auf dieses Event und ändern den DataContext
+		// von unserem FilmEditor grid auf den ausgewählten Film
+		private void FilmeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (FilmeListBox.SelectedItem is Film film)
+			{
+				FilmEditor.DataContext = film;
+			}
+		}
+	}
 }
